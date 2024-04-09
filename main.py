6992738af1b6223db2e972b98dcba11e6898bd58
Jayden <jayden.heberhendrickse@gmail.com>
2024-04-09 19:39:52 +0200

    Added Exception handling

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
 
-word = input("enter a word").upper()
-result = [nato_dict[letter] for letter in word]
-print(result)
+def convert_word():
+    word = input("enter a word").upper()
+    try:
+        result = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters please")
+        convert_word()
+    else:
+        print(result)
+
+
+convert_word()
